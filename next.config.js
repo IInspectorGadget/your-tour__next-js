1f69338d748789e7891badf8193880ca0c43b64b
Daniil Sivtsov <danilklin17@gmail.com>
2024-02-22 15:22:31 +0300

    change next.js.yml

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
-const path = require("path");
-
 module.exports = {
-  output: "export",
+  output: 'export',
   sassOptions: {
     includePaths: ["./src/styles/"],
     prependData: `@use "base/mixins.scss" as *;@use "base/fonts.scss" as *; @import "base/variables.scss"; `,
