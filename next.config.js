f277e2949fe2b41dbbbcb48a797615a33c405e0d
Daniil Sivtsov <danilklin17@gmail.com>
2024-02-22 18:30:04 +0300

    change config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = !!process.env.GITHUB_ACTIONS 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
 let assetPrefix = ''
 let output = ""
 if(isGithubActions){
-  assetPrefix = '',
+  assetPrefix = './',
   output = "export"
 }
 
 module.exports = {
-  output: "export",
+  output: output,
   sassOptions: {
     includePaths: ["./src/styles/"],
     prependData: `@use "base/mixins.scss" as *;@use "base/fonts.scss" as *; @import "base/variables.scss"; `,
