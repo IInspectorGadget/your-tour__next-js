80801748f74490d096a8427fd3f9a5a034485819
Daniil Sivtsov <danilklin17@gmail.com>
2024-02-22 15:07:12 +0300

    change package.json

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const path = require("path");
 
 module.exports = {
+  output: "export",
   sassOptions: {
     includePaths: ["./src/styles/"],
     prependData: `@use "base/mixins.scss" as *;@use "base/fonts.scss" as *; @import "base/variables.scss"; `,
