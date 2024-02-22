2cd1d825bda41dff0b0ad39e16a02cb8cd3bc9c1
Daniil Sivtsov <danilklin17@gmail.com>
2024-02-22 16:13:08 +0300

    change urls

--- src/data.js
+++ src/data.js
@@ -60,7 +60,7 @@ export const historyCards = [
             },
         ],
         detailLink: "#",
-        backgroundUrl: "/assets/images/story/story_photo-1.jpg"
+        backgroundUrl: "assets/images/story/story_photo-1.jpg"
     },
     {
         id: 2,
@@ -79,7 +79,7 @@ export const historyCards = [
         ],
 
         detailLink: "#",
-        backgroundUrl: "/assets/images/story/story_photo-2.jpg"
+        backgroundUrl: "assets/images/story/story_photo-2.jpg"
     },
     {
         id: 3,
@@ -102,7 +102,7 @@ export const historyCards = [
         ],
 
         detailLink: "#",
-        backgroundUrl: "/assets/images/story/story_photo-3.jpg"
+        backgroundUrl: "assets/images/story/story_photo-3.jpg"
     }
 ]
 
@@ -111,17 +111,17 @@ export const links = [
     {
         name: "instagram",
         link: "#",
-        iconUrl: "/assets/images/icons/inst.svg",
+        iconUrl: "assets/images/icons/inst.svg",
     },
     {
         name: "facebook",
         link: "#",
-        iconUrl: "/assets/images/icons/facebook.svg",
+        iconUrl: "assets/images/icons/facebook.svg",
     },
     {
         name: "vkontakte",
         link: "#",
-        iconUrl: "/assets/images/icons/vk.svg",
+        iconUrl: "assets/images/icons/vk.svg",
     }
 ]
 
@@ -129,60 +129,60 @@ export const links = [
 export const pictures = [
     {
         id: 1,
-        url: "/assets/images/travel/travel_photo-1.jpg",
+        url: "assets/images/travel/travel_photo-1.jpg",
     },
     {
         id: 2,
-        url: "/assets/images/travel/travel_photo-2.jpg",
+        url: "assets/images/travel/travel_photo-2.jpg",
         hidden: "tablet"
     },
     {
         id: 3,
-        url: "/assets/images/travel/travel_photo-3.jpg",
+        url: "assets/images/travel/travel_photo-3.jpg",
         hidden: "mobile"
     },
     {
         id: 4,
-        url: "/assets/images/travel/travel_photo-4.jpg",
+        url: "assets/images/travel/travel_photo-4.jpg",
     },
     {
         id: 5,
-        url: "/assets/images/travel/travel_photo-5.jpg",
+        url: "assets/images/travel/travel_photo-5.jpg",
     },
     {
         id: 6,
-        url: "/assets/images/travel/travel_photo-6.jpg",
+        url: "assets/images/travel/travel_photo-6.jpg",
         hidden: "mobile"
     },
     {
         id: 7,
-        url: "/assets/images/travel/travel_photo-7.jpg",
+        url: "assets/images/travel/travel_photo-7.jpg",
         hidden: "tablet"
     },
     {
         id: 8,
-        url: "/assets/images/travel/travel_photo-8.jpg",
+        url: "assets/images/travel/travel_photo-8.jpg",
     },
     {
         id: 9,
-        url: "/assets/images/travel/travel_photo-9.jpg",
+        url: "assets/images/travel/travel_photo-9.jpg",
     },
     {
         id: 10,
-        url: "/assets/images/travel/travel_photo-10.jpg",
+        url: "assets/images/travel/travel_photo-10.jpg",
     },
     {
         id: 11,
-        url: "/assets/images/travel/travel_photo-11.jpg",
+        url: "assets/images/travel/travel_photo-11.jpg",
     },
     {
         id: 12,
-        url: "/assets/images/travel/travel_photo-12.jpg",
+        url: "assets/images/travel/travel_photo-12.jpg",
         hidden: "mobile"
     },
     {
         id: 13,
-        url: "/assets/images/travel/travel_photo-13.jpg",
+        url: "assets/images/travel/travel_photo-13.jpg",
         hidden: "tablet"
     },
 ]
@@ -193,13 +193,13 @@ export const reviews = [
     {
         name: "Мария",
         tour: "Вдали от дома",
-        photo: "/assets/images/review/review_photo-1.jpg",
+        photo: "assets/images/review/review_photo-1.jpg",
         text: ["Идейные соображения высшего порядка, а также рамки и место обучения кадров обеспечивает широкому кругу (специалистов) участие в формировании новых предложений. Идейные соображения высшего порядка, а также начало повседневной работы по формированию позиции позволяет оценить значение модели развития."]
     },
     {
         name: "Павел", 
         tour: "Путешествие в горы",
-        photo: "/assets/images/review/review_photo-2.jpg",
+        photo: "assets/images/review/review_photo-2.jpg",
         text : [
             "Равным образом постоянный количественный рост и сфера нашей активности играет важную роль в формировании системы обучения кадров, соответствует насущным потребностям.",
             "Значимость этих проблем настолько очевидна, что дальнейшее развитие различных форм деятельности обеспечивает широкому кругу (специалистов) участие в формировании новых предложений. Повседневная практика показывает, что реализация намеченных плановых заданий в значительной степени обуславливает создание модели развития."
@@ -213,7 +213,7 @@ export const tours = [
     {
         id: 1,
         types: ["Популярные"],
-        imageUrl: "/assets/images/cards/card_tour_photo-1.jpg",
+        imageUrl: "assets/images/cards/card_tour_photo-1.jpg",
         url:"#",
         cost: "80 000",
         title: "Путешествие в горы",
@@ -221,7 +221,7 @@ export const tours = [
     {
         id: 2,
         types: ["Популярные"],
-        imageUrl: "/assets/images/cards/card_tour_photo-2.jpg",
+        imageUrl: "assets/images/cards/card_tour_photo-2.jpg",
         url:"#",
         cost: "80 000",
         title: "Путешествие в горы",
@@ -229,7 +229,7 @@ export const tours = [
     {
         id: 3,
         types: ["Популярные", "Велопрогулки"],
-        imageUrl: "/assets/images/cards/card_tour_photo-3.jpg",
+        imageUrl: "assets/images/cards/card_tour_photo-3.jpg",
         url:"#",
         cost: "80 000",
         title: "Путешествие в горы",
@@ -237,7 +237,7 @@ export const tours = [
     {
         id: 4,
         types: ["Популярные", "Авторский"],
-        imageUrl: "/assets/images/cards/card_tour_photo-4.jpg",
+        imageUrl: "assets/images/cards/card_tour_photo-4.jpg",
         url:"#",
         cost: "80 000",
         title: "Путешествие в горы",
@@ -245,7 +245,7 @@ export const tours = [
     {
         id: 5,
         types: ["Популярные", "Походы"],
-        imageUrl: "/assets/images/cards/card_tour_photo-5.jpg",
+        imageUrl: "assets/images/cards/card_tour_photo-5.jpg",
         url:"#",
         cost: "80 000",
         title: "Путешествие в горы",
@@ -253,7 +253,7 @@ export const tours = [
     {
         id: 6,
         types: ["Популярные", "Сплавы"],
-        imageUrl: "/assets/images/cards/card_tour_photo-6.jpg",
+        imageUrl: "assets/images/cards/card_tour_photo-6.jpg",
         url:"#",
         cost: "80 000",
         title: "Путешествие в горы",
