a4d16a519b0f7a8b2b0c1ae957a0438b0a2b6ea6
Daniil Sivtsov <danilklin17@gmail.com>
2024-02-22 15:27:18 +0300

    change next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: 'export',
+  images: {
+      unoptimized: true
+  },
   sassOptions: {
     includePaths: ["./src/styles/"],
     prependData: `@use "base/mixins.scss" as *;@use "base/fonts.scss" as *; @import "base/variables.scss"; `,
